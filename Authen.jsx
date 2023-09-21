21612f0dc65879dadbae2bbde37f1733d9c33306
Billy <abl.chakrit@gmail.com>
2023-09-21 22:16:39 +0700

    fix path

--- Authen.jsx
+++ Authen.jsx
@@ -1,6 +1,6 @@
 import App from "./src/App.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AddAlbum from "./src/AddAlbum.jsx";
+import AddAlbum from "./src/AddAlbum2.jsx";
 import SignInSide from "./src/SignInSide.jsx";
 import SignUp from "./src/SignUp.jsx";
 // import { useAppContext } from "./AppContext.jsx";
